a8bf34a41e8981656ed69b93ded25edafccce2be
Adam Telfer <adamtelfer@gmail.com>
2015-05-25 20:16:43 +0200

    Platformer first checkin

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public string EnemyTag = "Enemy";
+    public string CollectableTag = "Collectable";
+
+    public Transform currentCheckpoint;
+
+    public float bounceForceConstant = 1000f;
+    public Rigidbody physicsRigidBody;
+
+    public float edgeBuffer = 0.4f;
+
+    public float velocityDown = -5f;
+    public float velocityHorizontal = 5f;
+
+    public bool onlyOneCollision = false;
+
+    public Vector3 startingScale;
+
+    public float velocityConvertor;
+    public float velocityPower = 2f;
+    public float scaleVelocityMax;
+
+	// Use this for initialization
+	void Start () {
+        onlyOneCollision = false;
+        startingScale = transform.localScale;
+	}
+	
+    void OnCollisionEnter(Collision Col)
+    {
+        if (!onlyOneCollision && !CurrentlyBoostJumping())
+        {
+            Vector3 direction = (gameObject.transform.position - Col.gameObject.transform.position);
+            direction = direction.normalized;
+            Debug.Log(direction);
+            if (Mathf.Abs(direction.x) < edgeBuffer) {
+                physicsRigidBody.velocity = new Vector3(physicsRigidBody.velocity.x, 0, 0); // set y velocity to zero
+                physicsRigidBody.AddForce(new Vector3(0f, bounceForceConstant, 0f)); // some constant force here
+                onlyOneCollision = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (string.Compare(col.gameObject.tag,EnemyTag) == 0)
+        {
+            Debug.Log("Dead");
+            this.transform.position = currentCheckpoint.position;
+            this.physicsRigidBody.velocity = Vector3.zero;
+        } else if (string.Compare(col.gameObject.tag,CollectableTag) == 0)
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    bool CurrentlyBoostJumping()
+    {
+        return Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (CurrentlyBoostJumping())
+        {
+            physicsRigidBody.velocity = new Vector3(physicsRigidBody.velocity.x, velocityDown, 0f);
+        }
+        else
+        {
+            physicsRigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * velocityHorizontal, physicsRigidBody.velocity.y, 0f);
+        }
+	}
+
+    void LateUpdate()
+    {
+        float yScale = startingScale.y * Mathf.Min(1.0f + Mathf.Pow(Mathf.Abs(physicsRigidBody.velocity.y),velocityPower) * velocityConvertor, scaleVelocityMax);
+        this.transform.localScale = new Vector3(startingScale.x, yScale, startingScale.z);
+        onlyOneCollision = false;
+    }
+}
---
+++ Assets/SmoothFollow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+{
+
+    // Transform of target object, defaults to player
+    public Transform target;
+
+    public Vector3 offset = Vector3.zero;
+    // Time in seconds for smoothing
+    public float smoothTime = 0.5f;
+    // Distance from target
+    public float distance = 5.0f;
+
+    public float minimumY = 0.0f;
+    public float maximumY = 200f;
+
+    // Local copy of transform to reduce component lookups
+    Transform _transform;
+    // Velocity of camera smoothing
+    Vector3 _smoothVelocity;
+
+    void Start()
+    {
+        _transform = transform;
+
+        if (target == null)
+        {
+            // Point at player by default
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player != null)
+                target = player.transform;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            // Point camera towards target
+            Vector3 targetPosition = target.position + offset;
+
+            targetPosition = new Vector3(targetPosition.x, Mathf.Max(Mathf.Min(targetPosition.y, maximumY), minimumY), targetPosition.z);
+
+
+
+            targetPosition.z -= distance;
+
+            _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _smoothVelocity, smoothTime);
+        }
+    }
+}
